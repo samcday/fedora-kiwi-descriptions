ff1675d17ed73ab506547b16a770376f0117030d
Sam Day <me@samcday.com>
2025-02-22 15:40:38 +0100

    config.sh: Don't enable initial-setup.service for Phosh
    Signed-off-by: Sam Day <me@samcday.com>

--- config.sh
+++ config.sh
@@ -115,7 +115,7 @@ fi
 #--------------------------------------
 
 if [[ "$kiwi_profiles" == *"Disk"* ]]; then
-	if [[ "$kiwi_profiles" != *"GNOME"* ]]; then
+	if [[ "$kiwi_profiles" != *"GNOME"* ]] && [[ "$kiwi_profiles" != *"Phosh"* ]]; then
 		## Enable initial-setup
 		systemctl enable initial-setup.service
 		## Enable reconfig mode
